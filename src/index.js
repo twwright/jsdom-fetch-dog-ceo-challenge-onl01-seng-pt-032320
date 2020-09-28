a98ecfbe73cb5c2d63cd2dde8183439025f91a06
Thomas Wright <twwright@gmail.com>
2020-09-28 22:07:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+}
 
+function renderBooks(books) {
+  const main = document.querySelector('main')
+  books.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+
+
+function fetchDogs() {
+  return fetch("")
+}
 
 
 
